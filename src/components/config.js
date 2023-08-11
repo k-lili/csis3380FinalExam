63c526dd928f15bed6fbcd7823fb50350c809279
k-lili <lhlkarli@gmail.com>
2023-08-11 13:56:54 -0700

    karli - api update

--- src/components/config.js
+++ src/components/config.js
@@ -1,2 +1,3 @@
-const apiLink = 'http://localhost:5000/book';
+// const apiLink = 'http://localhost:5000/book';
+const apiLink = 'https://csis3380finalexambackend.onrender.com/book';
 module.exports = apiLink
